ab726bfc63273a99a72691288594ad5d618dddb8
reingalrt <adorufus@gmail.com>
2023-11-29 18:13:47 +0700

    finnal

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {
       // that is animation class
       animation: {
-        fade: 'fadeIn 0.5s ease-in-out',
+        fade: "fadeIn 0.5s ease-in-out",
+      },
+      backgroundColor: {
+        ezpz: "#E51F7E",
+      },
+      colors: {
+        ezpz: "#E51F7E",
       },
 
       // that is actual animation
-      keyframes: theme => ({
+      keyframes: (theme) => ({
         fadeIn: {
-          '0%': { backgroundColor: theme('colors.transparent') },
-          '100%': { backgroundColor: theme('colors.red.300') },
+          "0%": { backgroundColor: theme("colors.transparent") },
+          "100%": { backgroundColor: theme("colors.red.300") },
         },
       }),
     },
     container: {
-      padding: '50%',
+      padding: "50%",
     },
   },
   plugins: [],
-}
-
+};
